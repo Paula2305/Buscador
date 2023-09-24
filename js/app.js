ef797be5cd55f9b99621631f1cc169b3b62b665a
Paula2305 <polimoreno1976@gmail.com>
2023-09-24 16:55:18 -0300

    Creacion de un select para seleccionar el año

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 //Variables
 const resultado = document.querySelector('#resultado');
 
+const year = document.querySelector('#year');
 
+const max = new Date().getFullYear(); // agrega desde el año en el que te encontras
+const min = max - 10;
+
+console.log(max);
+console.log(min);
 
 //Eventos
 document.addEventListener('DOMContentLoaded', () => {
     mostrarAutos();
+
+    llenarSelect(); // Llena las opciones de años
+
 })
 
 
@@ -18,11 +27,25 @@ function mostrarAutos(){
         const autoHTML = document.createElement('p');
 
         autoHTML.textContent = ` 
-        ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmisión: ${transmisión} - Precio: ${precio} - Color: ${color}
+        ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmisión: ${transmision} - Precio: ${precio} - Color: ${color}
         `;
 
         // insertar en el html
         resultado.appendChild(autoHTML);
     });
+
+}
+
+// Genera los años del Select 
+function llenarSelect(){
+    // console.log('Llenando el select...');
+    for(let i = max; i >= min; i--){
+        // console.log(i);
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion); // Agrega las opciones de año
+
+    }
 
 }
